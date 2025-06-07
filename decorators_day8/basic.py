4309dc7d18bad74cdb4875e8fd67a4529e697283
Maximus Brutalis <dmitroid1sk@gmail.com>
2025-06-07 15:17:09 +0700

    Write decorator that count time of a func execution

--- decorators_day8/basic.py
+++ decorators_day8/basic.py
@@ -1,3 +1,4 @@
+"""
 def decorator(original_func):   # any name, needs original function as an argument
     def wrapper():              # necessary part, name is for convinience the same
         print("Start Game")
@@ -11,3 +12,22 @@ def middle_game():
     print("Middle Game")
 
 middle_game()
+"""
+
+import time
+
+def count_execution_time(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"{func.__name__} ran in {end-start:.2f} sec.")
+        return result
+    return wrapper
+
+@count_execution_time
+def slow_function():
+    time.sleep(2)
+    print("Function complete!")
+
+slow_function()
