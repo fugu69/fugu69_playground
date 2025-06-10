a568f4ed090367531eb3e2a4c1c90fca0b3d6f3d
Maximus Brutalis <dmitroid1sk@gmail.com>
2025-06-10 14:45:00 +0700

    Implement Jinja2 templating to render HTML pages with dynamic content

--- flask_day11/blog.py
+++ flask_day11/blog.py
@@ -2,14 +2,35 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+dummy_posts = [
+    {
+        "author": "Goofy",
+        "title": "Bark side of bagels",
+        "content": "Enjoy your bagels",
+        "date_posted": "May, 4, 1999",
+    },
+    {
+        "author": "Scoofy",
+        "title": "Argggh",
+        "content": "The Second Post Content",
+        "date_posted": "May, 4, 1999",
+    },
+    {
+        "author": "LKJJ",
+        "title": "Kkjjknjnin",
+        "content": "Jjhduaciauhncaisnk",
+        "date_posted": "May, 4, 1999",
+    }
+]
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", posts=dummy_posts)
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title="About")
 
 
 if __name__ == "__main__":
