9472e3d6480f6cc89e655b512e24c901686f0b38
Maximus Brutalis <dmitroid1sk@gmail.com>
2025-06-11 12:34:16 +0700

    Create forms.py with RegistrationForm, LoginForm and custom wtforms validator function to check special characters in an user password

---
+++ flask_day11/forms.py
@@ -0,0 +1,29 @@
+from flask_wtf import FlaskForm
+from wtforms import BooleanField, StringField, PasswordField, SubmitField
+from wtforms.validators import DataReqired, Length, Email, EqualTo, ValidationError
+
+def CheckSpecialCharacters():
+    def _check_special_chars(form, field):
+        special_characters = "!@#$%^&*()-_=+[]{}|;:',.<>?/`~"
+        if not any(c in special_characters for c in field.data):
+            raise ValidationError("Password must include at least one special character")
+    return _check_special_chars
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataReqired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password',
+                            validators=[DataRequired(), Length(min=4), CheckSpecialCharacters()])
+    confirm_password = PasswordField('Confirm password',
+                                    validators=[DataReqired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    user_identity = StringField('Username or Email',
+                           validators=[DataReqired(), Length(min=2, max=20)])
+    password = PasswordField('Password',
+                            validators=[DataRequired(), Length(min=4), CheckSpecialCharacters()])
+    remember_user = BooleanField('Remember Me')
+    submit = SubmitField('Login')
