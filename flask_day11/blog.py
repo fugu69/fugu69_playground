9f0676cc97d83939ab76f5739227c04336e8d62d
Maximus Brutalis <dmitroid1sk@gmail.com>
2025-06-12 13:01:30 +0700

    Set up User and Post models for blog_site.db

--- flask_day11/blog.py
+++ flask_day11/blog.py
@@ -1,6 +1,10 @@
 import os
+from datetime import datetime
 from dotenv import load_dotenv
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, DateTime, String, Integer, Text
+from sqlalchemy.sql import func
 from forms import RegistrationForm, LoginForm
 
 # Load environment variables from .env file
@@ -10,6 +14,54 @@ app = Flask(__name__)
 
 # Set secret key from environment variable
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+
+# db structure == Classes == Models
+db = SQLAlchemy(app)
+
+# Table row
+class User(db.Model):
+    # Table columns
+    id = Column(Integer, primary_key=True)
+    username = Column(String(20), unique=True, nullable=False)
+    email = Column(String(50), unique=True, nullable=False)
+    # Image will be hashed with 20 characters format -> String(20) format
+    image_file = Column(String(20), nullable=False, default="default.jpg")
+    # Password will be hashed with 60 characters format -> String(60) format
+    password = Column(String(60), nullable=False)
+
+    # A virtual list of Post objects on each User instance — user.posts
+    # A backref (reverse lookup) so that each Post can access its User via post.author
+    # "User has many posts" and "Each post has one author (user)"
+    posts = db.relationship("Post", backref="author", lazy=True)
+
+    # How the object is printed out
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+    
+class Post(db.Model):
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100), nullable=False)
+    created_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now()
+    )
+    updated_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now()
+    )
+    content = Column(Text, nullable=False)
+
+    # db.ForeignKey("user.id") -> Each Post must be linked to a User
+    # The column author_id in Post is a foreign key referencing User.id
+    # user is the name of the table created by User class
+    # "This post belongs to the user whose id matches author_id"
+    author_id = Column(Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.created_at}')"
+    
 
 # Temporary dammy data
 dummy_posts = [
